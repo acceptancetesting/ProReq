4a3a40badc59194ae1e6e106287a84ba4314abde
Avi <itsmeavi@gmail.com>
2024-11-18 10:56:01 +1100

    Requirement relationship

--- backend/src/requirements/requirements.module.ts
+++ backend/src/requirements/requirements.module.ts
@@ -7,11 +7,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Requirement } from './entities/requirement.entity';
 import { Project } from '../projects/entities/project.entity';
 import { UserProjectRolesModule } from '../user-project-roles/user-project-roles.module'; // Import the module
+import { RequirementRelationshipModule } from '../requirementrelations/requirementRelationship.module'; // Import the module
+import { RequirementRelationship } from '../requirementrelations/entities/requirementRelationship.entity'; // Import the module
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Requirement, Project]),
+    TypeOrmModule.forFeature([Requirement, Project, RequirementRelationship]),
     UserProjectRolesModule, // Import the module here
+    RequirementRelationshipModule, // Import relationship module
   ],
   controllers: [RequirementsController],
   providers: [RequirementsService],
